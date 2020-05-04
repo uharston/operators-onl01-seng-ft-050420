070418f7d8bd00375c6a1916554ad0653a2fa032
Uriah Harston <uharston@gmail.com>
2020-05-04 23:23:00 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 def unsafe?(speed)
 if speed > 60 && speed < 40
 	true
-else 
+else
 	false
 	end
 end
@@ -11,6 +11,3 @@ end
 def not_safe?(speed)
 	speed > 60 && speed < 40 ? true : false
 end
-	
-
-
